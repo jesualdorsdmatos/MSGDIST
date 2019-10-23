5d3a90c34b28ef1670555ed5db5fe93f8b249856
jesualdorsdm <jesualdorsdm@jesualdorsdm-VirtualBox>
2019-10-23 14:39:37 +0100

    teste

---
+++ MSGDISTSO/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jesualdorsdm
+ *
+ * Created on 23 de outubro de 2019, 14:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("teste!!!");
+    return (EXIT_SUCCESS);
+}
+
