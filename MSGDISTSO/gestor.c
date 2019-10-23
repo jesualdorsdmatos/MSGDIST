99bdb7cdf7e9a1ce5325de6b84fb6f1fb1a36ab5
chicosilva99 <franciscosilvaeu15@gmail.com>
2019-10-24 00:44:47 +0100

    xico take 2

--- MSGDISTSO/gestor.c
+++ MSGDISTSO/gestor.c
@@ -1,32 +1,43 @@
-/**/
+/*
+ 
+ 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
+
+#include "structs.h"
+
+
+
+
+void encerrar(){
+    printf("O programa vai encerrar!\n");
+    exit(0);
+}
 int main(int argc, char *argv[]){
+    varamb var;
+    char *str;
     // Processamento das variáveis de amabientes
     //Máximo de mensagens
-     char* str;
-     str=getenv("MAXMSG");
-     if(str!=NULL)
-         printf("A 1 variavel tem o valor %s .\n",str);
-     else    
-       printf("A variavel MAXMSG nao existe.\n");
-        
+    if((getenv("MAXMSG"))==NULL)
+         printf("A variavel MAXMSG nao existe.\n");
+     else   
+         var.MAXMSG=atoi((getenv("MAXMSG")));
+         
      //Máximo número de palavras rejeitadas
-     str= getenv("MAXNOT");
-     if(str != NULL)
-         printf("A variavel 2 tem o valor. %s \n",str);
-     else    
-       printf("A variavel MAXNOT nao existe.\n");
+    if((getenv("MAXNOT"))==NULL)
+         printf("A variavel MAXNOT nao existe.\n");
+     else   
+         var.MAXNOT=atoi((getenv("MAXNOT")));
        
      //Nome da ficheiro com as palavras proíbidas
-     str= getenv("WORDSNOT");
-     if(str != NULL)
-         printf("A variavel 3 tem o valor. %s \n",str);
-     else    
-       printf("A variavel WORDSNOT nao existe.\n");
+     if((getenv("WORDSNOT"))==NULL)
+         printf("A variavel WORDSNOT nao existe.\n");
+     else   
+         var.WORDSNOT=atoi((getenv("WORDSNOT")));
      
     char cmd[50];
     
@@ -34,25 +45,14 @@ int main(int argc, char *argv[]){
     printf("Intoduza um comando: ");
     scanf("%s",cmd);
             
-    
-    
-    
     for(int i =0; i<strlen(cmd);i++)
         cmd[i]= toupper(cmd[i]);
     
-    
- if (strcmp(cmd,"SHUTDOWN")==0){
-        printf("O Programa vai encerrar.\n");
-        exit(0);
+
+    if (strcmp(cmd,"SHUTDOWN")==0){
+       encerrar();
     }else if(strcmp(cmd,"FILTER")==0){
-        char *pal;
-        scanf("%s",pal);
-        if(fork()==0){
-            execlp("./verificador","./verificador",pal,NULL);
-            exit(0);
-        }
-        
-        
+    
     }else if(strcmp(cmd,"USERS")==0){
     
     } else if(strcmp(cmd,"TOPICS")==0){
