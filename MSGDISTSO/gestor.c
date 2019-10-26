71cc58be8f93ca83ce0240e7b56e9b91ea971db2
jesualdorsdmatos <a21250379@isec.pt>
2019-10-26 01:00:40 +0100

    versao final!

--- MSGDISTSO/gestor.c
+++ MSGDISTSO/gestor.c
@@ -1,20 +1,22 @@
-#include "geraldefinc.h"
 #include "gestor_default.h"
+#include "geraldefinc.h"
 
 
-
-
-
-void encerrar(){
-    printf("O programa vai encerrar!\n");
-    exit(0);
+void removerespaco(char str[]) {
+    int j = 1;
+    for (int i = 1; str[i]; i++) {
+        if (str[i] != ' ' || (str[i - 1] != ' ')) {
+           str[j] = str[i];
+           j++;
+        }
+    }
+    str[j] = '\0';
 }
-int main(int argc, char *argv[]){
+
+
+
+varamb lervarambiente(){
     varamb var;
-    char *str;
-    int estado=0;
-    // Processamento das variáveis de amabientes
-    //Máximo de mensagens
     if((getenv("MAXMSG"))==NULL){
          printf("MAXMSG default.\n");
     var.MAXMSG=MAXMSGD;
@@ -33,32 +35,48 @@ int main(int argc, char *argv[]){
      if((getenv("WORDSNOT"))==NULL){
          printf("WORDSNOT default.\n");
         strcpy(var.WORDSNOT,WORSNOTD);
+        
      }
      else   
          strcpy(var.WORDSNOT,"WORDSNOT");
-     
+    
+    return var;
+}
+
+void encerrar(){
+    printf("O programa vai encerrar!\n");
+    exit(0);
+}
+int main(int argc, char *argv){
+      char *str;
+    int estado=0;
     char cmd[50];
     char pal[50];
+    cmds b;
+    b.argumento=NULL;
+    
+    varamb var=lervarambiente();
+  
+   
+
     
     while(1){
-<<<<<<< HEAD
     printf("Intoduza um comando: ");
-    scanf("%s",cmd);
-            
-=======
-    printf("Intoduza um comando:");
     scanf(" %[^\n]",cmd);
     removerespaco(cmd);
 
    
->>>>>>> ramo_jesualdo
     for(int i =0; i<strlen(cmd);i++)
         cmd[i]= toupper(cmd[i]);
-    
 
-    if (strcmp(cmd,"SHUTDOWN")==0){
+        b.comando=strtok(cmd," ");
+        b.argumento=strtok(NULL, " ");
+
+
+    if (strcmp(b.comando,"SHUTDOWN")==0){
        encerrar();
-    }else if(strcmp(cmd,"FILTER")==0){
+    }else if(strcmp(b.comando,"FILTER")==0 && b.argumento!=NULL){
+        if(strcmp(b.argumento,"ON")==0){
          scanf("%s",pal);
         
          if(fork()==0){
@@ -66,41 +84,28 @@ int main(int argc, char *argv[]){
              exit(1);
          }
          wait(NULL);
-<<<<<<< HEAD
-=======
         }else if(strcmp(b.argumento,"OFF")==0){
 
-        }elsegit add *
-        git commit -m "comentário"
-        git push
-        git checkout master
-        git pull
-        git marge ramo_name
-        git commit -m "comentario"
-        git push
-        volto ao meu ramo
-        git marge master
-        git push
+        }else
             printf("[ERRO] Argumento:%s invalido.\nArgumento:ON/OFF\n",b.argumento);
     }else if(strcmp(b.comando,"USERS")==0){
->>>>>>> ramo_jesualdo
     
-    }else if(strcmp(cmd,"USERS")==0){
+    } else if(strcmp(b.comando,"TOPICS")==0){
     
-    } else if(strcmp(cmd,"TOPICS")==0){
+    } else if(strcmp(b.comando,"MSG")==0){
     
-    } else if(strcmp(cmd,"MSG")==0){
+    } else if(strcmp(b.comando,"DEL")==0 && b.argumento!=NULL){
     
-    } else if(strcmp(cmd,"DEL")==0){
+    } else if(strcmp(b.comando,"KICK")==0 && b.argumento!=NULL){
+        printf("DEBG!!\n");
     
-    } else if(strcmp(cmd,"KICK")==0){
-    
-    } else if(strcmp(cmd,"PRUNE")==0){
+    } else if(strcmp(b.comando,"PRUNE")==0){
     
     } else{
-        printf("o comando %s nao e valido\n",cmd);
+        printf("[ERRO] Comando %s invalido ou falta de argumentos!\n",cmd);
     }
     //pause();
   }
+
 
 }
