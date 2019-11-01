7f96825aa591b75d57616b67ecfc302882ecedbc
jesualdorsdmatos <a21250379@isec.pt>
2019-11-01 00:08:51 +0000

    Trabalho quase concluido, o ler palavra digitada funciona mas no shutdown da erro de memoria, nao sei ao certo ainda o motivo. Implementado o comando mensagem para digitar a palavra, a ligacao entre verificador e gestor funciona na integridade!

--- MSGDISTSO/gestor.c
+++ MSGDISTSO/gestor.c
@@ -1,128 +1,189 @@
 #include "gestor_default.h"
 #include "geraldefinc.h"
 
-// Retira espa�os a mais entre os comandos
-void removerespaco(char str[]) {
+
+void imprimirin(){
+    printf("************************************************\n");
+    printf("**       BEM-VINDO AO GESTOR DO MSGDIST       **\n");
+    printf("**Comando:[help] para listar todos os comandos**\n");
+    printf("************************************************\n");
+}
+
+
+void imprimirfi(){
+    printf("*************************************************\n");
+    printf("**           Trabalho realizado por:           **\n");
+    printf("**       Jesualdo Matos/Francisco Silva        **\n");
+    printf("*************************************************\n");
+}
+
+// Retira espacos a mais entre os comandos
+void removerespaco(char str[])
+{
     int j = 0;
-    for (int i = 0; str[i]; i++) {
-        if (str[i] != ' ' || (str[i - 1] != ' ')) {
-           str[j] = str[i];
-           j++;
+    for (int i = 0; str[i]; i++)
+    {
+        if (str[i] != ' ' || (str[i - 1] != ' '))
+        {
+            str[j] = str[i];
+            j++;
         }
     }
     str[j] = '\0';
 }
 
-
-
-varamb lervarambiente(){
+varamb lervarambiente()
+{
     varamb var;
-    if((getenv("MAXMSG"))==NULL){
-         printf("MAXMSG default.\n");
-    var.MAXMSG=MAXMSGD;
+    //Maximo de mensagems
+    if ((getenv("MAXMSG")) == NULL)
+    {
+        printf("MAXMSG default.\n");
+        var.MAXMSG = MAXMSGD;
     }
-     else   
-         var.MAXMSG=atoi((getenv("MAXMSG")));
-         
-     //M�ximo n�mero de palavras rejeitadas
-    if((getenv("MAXNOT"))==NULL){
-         printf("MAXNOT default.\n");
-    var.MAXNOT=MAXNOTD;}
-     else   
-         var.MAXNOT=atoi((getenv("MAXNOT")));
-       
-     //Nome da ficheiro com as palavras pro�bidas
-     if((getenv("WORDSNOT"))==NULL){
-         printf("WORDSNOT default.\n");
-        strcpy(var.WORDSNOT,WORSNOTD);   
-     }
-     else   
-         strcpy(var.WORDSNOT,"WORDSNOT");
-    
+    else
+        var.MAXMSG = atoi((getenv("MAXMSG")));
+
+    //M�ximo n�mero de palavras rejeitadas
+    if ((getenv("MAXNOT")) == NULL)
+    {
+        printf("MAXNOT default.\n");
+        var.MAXNOT = MAXNOTD;
+    }
+    else
+        var.MAXNOT = atoi((getenv("MAXNOT")));
+
+    //Nome da ficheiro com as palavras pro�bidas
+    if ((getenv("WORDSNOT")) == NULL)
+    {
+        printf("WORDSNOT default.\n");
+        strcpy(var.WORDSNOT, WORSNOTD);
+    }
+    else
+        strcpy(var.WORDSNOT, "WORDSNOT");
+
     return var;
 }
 
-void encerrar(){
+void encerrar()
+{
     printf("O programa vai encerrar!\n");
+    imprimirfi();
     exit(0);
 }
-int main(int argc, char *argv){
-    char cmd[50],pal[50];
+
+int main(int argc, char *argv)
+{
+    char cmd[50], pal[200];
     cmds b;
-    b.argumento=NULL;
-    int p[2],res;
-    varamb var=lervarambiente();
-
-
-    
-    while(1){
-    printf("Intoduza um comando: ");
-    scanf(" %[^\n]",cmd);
-    fflush(stdin);
-    removerespaco(cmd);
-
-    for(int i =0; i<strlen(cmd);i++)
-        cmd[i]= toupper(cmd[i]);
-
-        b.comando=strtok(cmd," ");
-        b.argumento=strtok(NULL, " ");-
-
-    if (strcmp(b.comando,"SHUTDOWN")==0){
-       encerrar();
-    }else if(strcmp(b.comando,"FILTER")==0 && b.argumento!=NULL){
-        if(strcmp(b.argumento,"ON")==0){
-            //scanf("%s",pal);
-            pipe(p);//O pipe tem de ser criado antes do fork
-                 //para quando este for feito herdar a tabela de fd's
-            getchar();
-                fgets(pal,50,stdin);
-         if(fork()==0){
-            /* p[0]-lado de escrita do pipe;//0-stdin;
-             1-stdout;
-             2-stderr;
-             //p[0]-lado de escrita do pipe;
-             //p[1]- lado de leitura do pipe;
-*/
-              
-              close(0);    //fechamos o teclado
-              dup(p[0]);  // trocas o teclado pela extremidade p[0] do pipe.
-              close(p[0]);//fechamos a extremidade do pipe para que n�o seja ecrito mais nada.
-              close(p[1]);// fechamos a extremidade de leitura do pipe.
-             execl("verificador","verificador",var.WORDSNOT,NULL);
-             exit(1);
-         }
-         close(1);//fechamos ecr�
-         dup(p[1]);
-         close(p[1]);
-         close (p[0]);
-         //FILE* fd=fdopen(p[1],"w");
-         //char *c;
-         //fgets(c,1,fd);
-         printf("%s",pal);
-         close(1);
-         wait(NULL);
-        }else if(strcmp(b.argumento,"OFF")==0){
-
-        }else
-            printf("[ERRO] Argumento:%s invalido.\nArgumento:ON/OFF\n",b.argumento);
-    }else if(strcmp(b.comando,"USERS")==0){
-    
-    } else if(strcmp(b.comando,"TOPICS")==0){
-    
-    } else if(strcmp(b.comando,"MSG")==0){
-    
-    } else if(strcmp(b.comando,"DEL")==0 && b.argumento!=NULL){
-    
-    } else if(strcmp(b.comando,"KICK")==0 && b.argumento!=NULL){
-        printf("DEBG!!\n");
-    
-    } else if(strcmp(b.comando,"PRUNE")==0){
-    
-    } else{
-        printf("[ERRO] Comando %s invalido ou falta de argumentos!\n",cmd);
+    char buffer[3];
+    b.argumento = NULL;
+    int pipe1[2];
+    int pipe2[2];
+    int filter = 0;
+    int numerx = 0;
+    imprimirin();
+    varamb var = lervarambiente();
+    if (pipe(pipe1) == -1)
+    {
+        fprintf(stderr, "[ERRO] Criacao pipe1\n");
+        return 1;
     }
-    //pause();
-  }
+    if (pipe(pipe2) == -1)
+    {
+        fprintf(stderr, "[ERRO] Criacao pipe2\n\n");
+        return 1;
+    }
 
+    if (fork() == 0)
+    {
 
+        close(pipe1[WRITE]);
+        close(pipe2[READ]);
+        dup2(pipe1[READ], READ);
+        dup2(pipe2[WRITE], WRITE);
+        close(pipe1[READ]);
+        close(pipe2[WRITE]);
+
+        execl("verificador", "verificador", var.WORDSNOT, NULL);
+    }
+    while (1)
+    {
+        printf("Intoduza um comando: ");
+        scanf(" %[^\n]", cmd);
+        fflush(stdin);
+        removerespaco(cmd);
+
+        for (int i = 0; i < strlen(cmd); i++)
+            cmd[i] = toupper(cmd[i]);
+
+        b.comando = strtok(cmd, " ");
+        b.argumento = strtok(NULL, " ");
+
+        if (strcmp(b.comando, "SHUTDOWN") == 0)
+        {   
+            encerrar();
+        }
+        else if (strcmp(b.comando, "FILTER") == 0 && b.argumento != NULL)
+        {
+            if (strcmp(b.argumento, "ON") == 0)
+            {
+                filter = 1;
+            }
+            else if (strcmp(b.argumento, "OFF") == 0)
+            {
+                filter = 0;
+            }
+            else
+                printf("[ERRO] Argumento:%s invalido.\nArgumento:ON/OFF\n", b.argumento);
+        }
+        else if (strcmp(b.comando, "USERS") == 0)
+        {
+        }
+        else if (strcmp(b.comando, "TOPICS") == 0)
+        {
+        }
+        else if (strcmp(b.comando, "MSG") == 0)
+        {
+        }
+        else if (strcmp(b.comando, "MENSAGEM") == 0)
+        {
+            printf("Introduz uma mensagem:");
+            getchar();
+            fgets(pal, sizeof(pal), stdin);
+            if (filter == 1)
+            {
+                close(pipe1[READ]);
+                close(pipe2[WRITE]);
+                if (write(pipe1[WRITE], pal, sizeof(pal)) != sizeof(pal))
+                    fprintf(stderr, "[ERRO] Envio da mensagem!\n");
+
+                write(pipe1[WRITE], "\n", strlen("\n"));
+
+                if (write(pipe1[WRITE], "##MSGEND##\n", sizeof("##MSGEND##\n")) != sizeof("##MSGEND##\n"))
+                    fprintf(stderr, "[ERRO] Envio do (##MSGEND##)\n");
+
+                int nbytes = read(pipe2[READ], buffer, sizeof(buffer));
+                if (nbytes <= 0)
+                    fprintf(stderr, "[ERRO] A Ler do pipe2\n");
+                else
+                    numerx = atoi(buffer);
+
+                printf("Numero de palavras invalidas:%d\n", numerx);
+            }
+        }
+        else if (strcmp(b.comando, "DEL") == 0 && b.argumento != NULL)
+        {
+        }
+        else if (strcmp(b.comando, "KICK") == 0 && b.argumento != NULL)
+        {
+        }
+        else if (strcmp(b.comando, "PRUNE") == 0)
+        {
+        }
+        else
+        {
+            printf("[ERRO] Comando %s invalido ou falta de argumentos!\n", cmd);
+        }
+    }
 }
